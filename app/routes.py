192e6f160a6af8967b07cb5d2ad88227f57a37d0
corronwarren2 <86121210+corronwarren2@users.noreply.github.com>
2021-09-24 02:04:16 -0400

    upload

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from app import app, db
+from flask import render_template, redirect, url_for, flash
 from app.forms import RegisterForm
-from app import app
-from flask import render_template
+from app.models import User
 
 
 @app.route('/')
@@ -12,10 +13,25 @@ def index():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
+    print(form.username)
     if form.validate_on_submit():
         username = form.username.data
         email = form.email.data
         password = form.password.data
         print(username, email, password)
+
+        #create new instance of User
+        new_user = User(username, email, password)
+
+        #add new user to
+        db.session.add(new_user)
+        db.session.commit()
+
+        # once mew_user os added to db, flash success message
+        flash(f'Thank you for signing up {new_user.username}!', 'primary')
+
+        #redirect user back to home page
+        return redirect(url_for('index'))
+        
     return render_template('register.html', title="Register for CT Blog", form=form)
     
